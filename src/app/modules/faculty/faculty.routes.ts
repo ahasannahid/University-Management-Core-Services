92ab1e494c9b68d4b881318f9f9779f2c63045eb
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-08-23 16:55:04 +0600

    update and delete faculty

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -16,5 +16,18 @@ router.post(
     FacultyController.insertIntoDB
 );
 
+router.patch(
+    '/:id',
+    validateRequest(FacultyValidation.update),
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    FacultyController.updateOneInDB
+);
+
+router.delete(
+    '/:id',
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    FacultyController.deleteByIdFromDB
+);
+
 
 export const facultyRoutes = router;
